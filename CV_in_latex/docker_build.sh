1788a1b34195ba47dec15862d6fdf3c66f1488ae
cricrispysquid <shackelten@gmail.com>
2026-05-10 00:49:29 +0800

    Delete CV_in_latex directory

--- CV_in_latex/docker_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build the Docker image
-docker compose build
-
-# Run the container to generate the PDF
-docker compose run --rm cv-builder
-
-echo "CV built successfully. PDF file is available as main.pdf" 
